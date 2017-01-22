04d3221535f6ce2441f53a03922513a53f23d0c9
Pedro Netto <pedro@brisa.is>
2017-01-22 19:48:17 +0000

    Add base files for the git introduction class

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # git-introduction
-Sample code for our GIT Introduction class
+GIT introduction class
+
+Pedro Netto
+pedro@brisa.is (feel free to shoot me a message in case you have questions after the class)
+
+---
+
+:: Help!
+- GIT Documentation https://git-scm.com/doc
+- This page brings easy to understand explanations
+  about the many new terms GIT has. Scroll to
+  "Cheat Sheet": https://recompilermag.com/issues/issue-1/how-to-teach-git/
+              
+:: Further learning
+- Exercises! http://jnthn.net/training/git/exercises.pdf
+              
+- Scott Chacon (Github) speaks about GIT and goes through what GIT does 
+  in the background (he speaks REALLY fast though) 
+  https://www.youtube.com/watch?v=ZDR433b0HJY
+
+- Always good to check Wikipedia for a general overview 
+  https://en.wikipedia.org/wiki/Git
---
+++ scripts.js
@@ -0,0 +1,43 @@
+/**
+ * A polite function that says hello in multiple languages
+ * @param {String} language
+ */
+function sayHello(language) {
+    
+    // Defaults to Icelandic
+    if (!language) {
+        language = 'is';
+    }
+    
+    // Built-in dictionary
+    var localized = {
+        'is': 'Hæ',
+        'en': 'Hello',
+        'pt': 'Oi',
+    };
+    
+    // Handle non-existing languages
+    if (!(language in localized)) {
+        alert('There is no support for : "' + language + '", but in Iceland we say ' + localized['is']);
+        return;
+    }
+    
+    alert(localized[language]);
+    
+}
+
+/**
+ * Create our event handler after the DOM is loaded
+ * (otherwise it the script would not find the button or the language menu)
+ */
+window.onload = function () {
+    
+    var button = document.getElementById('sayHelloBtn'),
+        languageMenu = document.getElementById('languageMenu');
+    
+    // When clicked, say Hello in selected language
+    button.addEventListener('click', function () {
+        sayHello(languageMenu.value);
+    });
+    
+}
